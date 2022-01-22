b92870326aef3e843aa3429bb35c5605581bb391
elkd <el@obrisk.com>
2022-01-22 15:34:39 +0800

    implemented switch mechanism to popup windows to target elements outside DOM

--- autopost.py
+++ autopost.py
@@ -3,32 +3,115 @@ from time import sleep
 
 from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
+from selenium.common.exceptions import (
+    UnexpectedAlertPresentException,
+    NoSuchElementException,
+    TimeoutException,
+    ElementNotInteractableException
+)
+from pages import LoginPage, clear_complete_profile_popup, clear_notif_popup
 
-from pages import LoginPage
 
 load_dotenv()
 
-
-
-def login():
-    #browser = webdriver.Firefox()
-    #browser = webdriver.Chrome()
-    browser = uc.Chrome()
-    browser.implicitly_wait(5)
-
+def login(browser):
     login_page = LoginPage(browser)
 
-    username = os.getenv('USERNAME')
-    password = os.getenv('PASSWORD')
+    username = os.getenv('ETOROUSER')
+    password = os.getenv('ETOROPASSWORD')
     login_page.login(username, password)
 
+
+def upload_post(browser):
+    sleep(10)
+
     try:
-        errors = browser.find_elements_by_css_selector('#error_message')
+        wait = WebDriverWait(browser, 90)
+
+        open_postform_btn = wait.until(ec.visibility_of_element_located((
+            By.CSS_SELECTOR,
+            'button.button-text.et-font-m'
+        )))
+        open_postform_btn.click()
+
+        sleep(10)
+        content_input = wait.until(ec.visibility_of_element_located((
+            By.CSS_SELECTOR,
+            'textarea.write-post-textarea.ng-pristine.ng-valid.ng-touched'
+        )))
+
+        #content_input.send_keys(20 * Keys.BACKSPACE)
+        content_input.send_keys('Here is the sample content')
+        sleep(7)
+        content_input.send_keys(7 * Keys.BACKSPACE)
+        sleep(13)
+        content_input.send_keys('is the content')
+        sleep(14)
+        upload_input = browser.find_element_by_class_name("form-upload-photo-label")
+        upload_input.send_keys("./pics/sample.jpg")
+        sleep(34)
+
+        wait.until(ec.element_to_be_clickable((By.CLASS_NAME, "write-post-button"))).click()
+
+        sleep(60)
+        print('hooray')
+        print('hooray')
+        #self.browser.get('https://www.etoro.com/accounts/logout/')
+
+    except (
+            TimeoutException,
+            UnexpectedAlertPresentException,
+            NoSuchElementException,
+            ElementNotInteractableException )  as e:
+
+        print(f'Eroto Known Exception Captured {e}')
+        clear_complete_profile_popup(browser)
+
+        browser.execute_script("window.scrollBy(0,200)","")
+        #wait.until_not(ec.visibility_of_element_located((By.ID, "cdk-overlay-0")))
+        browser.refresh()
+        sleep(10)
+        upload_post(browser)
+
     except Exception as e:
-        print(f'Cannot catch Errors: {e}')
-    sleep(300)
+        print(f'Unhandled Exception Happened: {e}')
+        print(f'THE BROWSER WILL EXIT IN 60 SECONDS')
+        print(f'THE BROWSER WILL EXIT SOONER...')
+        sleep(60)
+
+
+def run_upload_task():
+    options = uc.ChromeOptions()
+
+    # setting profile
+    #options.user_data_dir = "c:\\temp\\profile"
+
+    # just some options passing in to skip annoying popups
+    options.add_argument('--no-first-run --no-service-autorun --password-store=basic')
+    #browser = webdriver.Firefox()
+    #browser = webdriver.Chrome()
+    browser = uc.Chrome(options=options)
+    browser.implicitly_wait(60)
+    browser.maximize_window()
+
+    #browser.get('https://etoro.com/home/')
+    #try: browser.find_element_by_css_selector('home-element').click()
+    #except NoSuchElementException as e: pass
+    try:
+        login(browser)
+    except TimeoutException as e:
+        sleep(30)
+        login(browser)
+        print(e)
+
+    browser.execute_script("window.scrollBy(0,300)","")
+    upload_post(browser)
     browser.close()
 
+
 if __name__ == '__main__':
-    login()
+    run_upload_task()
--- pages.py
+++ pages.py
@@ -1,5 +1,69 @@
 from time import sleep
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import (
+    NoSuchElementException, ElementNotInteractableException
+)
+
+
+def clear_complete_profile_popup(browser):
+    try:
+        # storing the current window handle to get back to dashboard
+        main_page = browser.current_window_handle
+
+        print(browser.window_handles)
+        print(len(browser.window_handles))
+        # changing the handles to access login page
+        profile_page = None
+        for handle in browser.window_handles:
+            if handle != main_page:
+                profile_page = handle
+
+        # change the control to signin page       
+        browser.switch_to.window(profile_page)
+
+        pop_modal = WebDriverWait(browser, 60).until(ec.visibility_of_element_located((By.ID, "cdk-overlay-0")))
+
+        #neverask_check = browser.find_element_by_css_selector('input#cbTradeModal')
+        #browser.execute_script("arguments[0].click();", neverask_check)
+        sleep(5)
+        close_btn = browser.find_element_by_css_selector('button.default-font-icon.icon-close')
+        browser.execute_script("arguments[0].click();", close_btn)
+
+        sleep(5)
+        #change control to main page
+        browser.switch_to.window(main_page)
+        return True
+    except (NoSuchElementException, ElementNotInteractableException) as e:
+        print(f'Clear Func has Exception {e}')
+        sleep(5)
+    return None
+
+
+def clear_notif_popup(browser):
+    try:
+        # storing the current window handle to get back to dashboard
+        main_page = browser.current_window_handle
+
+        # changing the handles to access login page
+        for handle in browser.window_handles:
+            if handle != main_page:
+                notif_page = handle
+
+        # change the control to signin page       
+        browser.switch_to.window(notif_page)
+
+        close_btn = browser.find_element_by_css_selector('a.link.pre-push-popup-link')
+        browser.execute_script("arguments[0].click();", close_btn)
+        #change control to main page
+        browser.switch_to.window(main_page)
+        return True
+    except NoSuchElementException as e:
+        print(f'Clear Func has Exception {e}')
+        sleep(5)
+    return None
 
 
 class LoginPage:
