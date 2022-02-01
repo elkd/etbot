b032ff984d3b0e596dfab492f87a23067b5747fb
elkd <el@obrisk.com>
2022-02-01 12:23:02 +0800

    added report model to the admin dashboard

--- autopost/admin.py
+++ autopost/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from autopost.models import EtoroUser, ScheduledPost
+from autopost.models import EtoroUser, ScheduledPost, UploadReport
 
 admin.site.register(ScheduledPost)
 admin.site.register(EtoroUser)
+admin.site.register(UploadReport)
