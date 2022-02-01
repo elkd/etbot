cd815d043e14426cec9c868f4f8ae57b987f0c00
elkd <el@obrisk.com>
2022-02-01 16:04:51 +0800

    fixed the https config errors

--- etbot/settings.py
+++ etbot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import logging
 import environ
 import requests
 from pathlib import Path
@@ -55,9 +56,40 @@ if DEBUG is False:
             proc.wait()
 
 
+    # SECURITY
+    # ------------------------------------------------------------------------------
+    # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
+    SECURE_SSL_REDIRECT = env.bool('SECURE_SSL_REDIRECT', default=True)
+    # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
     SESSION_COOKIE_SECURE = True
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+    # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-httponly
+    SESSION_COOKIE_HTTPONLY = True
+    # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
     CSRF_COOKIE_SECURE = True
+    # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-httponly
+    CSRF_COOKIE_HTTPONLY = True
+    # This is not so important but added here to avoid confusion
+    # when generating CSRF token on Ajax requests
+    #https://docs.djangoproject.com/en/2.2/ref/csrf/#django.views.decorators.csrf.ensure_csrf_cookie
+    #https://docs.djangoproject.com/en/2.2/ref/settings/#std:setting-CSRF_USE_SESSIONS
+    CSRF_USE_SESSIONS = True
+
+    # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
+    # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
+    # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
+    SECURE_HSTS_SECONDS = 120
+    # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool('SECURE_HSTS_INCLUDE_SUBDOMAINS', default=True)
+    # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-preload
+    SECURE_HSTS_PRELOAD = env.bool('SECURE_HSTS_PRELOAD', default=True)
+    # https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
+    SECURE_CONTENT_TYPE_NOSNIFF = env.bool('SECURE_CONTENT_TYPE_NOSNIFF', default=True)
+    # https://docs.djangoproject.com/en/dev/ref/settings/#secure-browser-xss-filter
+    SECURE_BROWSER_XSS_FILTER = True
+    # https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
+    X_FRAME_OPTIONS = 'DENY'
 
 
 try:
@@ -66,7 +98,8 @@ except KeyError as e:
     raise RuntimeError("Could not find a SECRET_KEY in environment") from e
 
 
-ALLOWED_HOSTS = ['.babylonbot.link', 'localhost']
+AWS_LOCAL_IP = get_ec2_instance_ip()
+ALLOWED_HOSTS = [AWS_LOCAL_IP, 'www.babylonbot.link', 'babylonbot.link']
 
 # Application definition
 
@@ -200,6 +233,29 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 1300000000
 # REDIS setup
 REDIS_URL = 'redis://localhost:6379'
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": [
+            REDIS_URL,
+            #env('SLAVE_REDIS_URL'),
+        ],
+        "OPTIONS": {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            # Mimicing memcache behavior.
+            # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
+            'IGNORE_EXCEPTIONS': True,
+        }
+    }
+}
+
+
+#SESSION
+#Improve performance #Support multiple servers
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+
+CELERY_BROKER_URL = REDIS_URL
 
 CELERY_TIMEZONE = 'Europe/Paris'
