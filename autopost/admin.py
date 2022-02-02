5715eebf1c7867113d6c8d117fca9b13d27fc6ff
elkd <el@obrisk.com>
2022-02-02 15:05:02 +0800

    added the link to create new etoro users, removed django users, groups from the admin

--- autopost/admin.py
+++ autopost/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group, Site
 from autopost.models import EtoroUser, ScheduledPost, UploadReport
+
 
 admin.site.register(ScheduledPost)
 admin.site.register(EtoroUser)
 admin.site.register(UploadReport)
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.unregister(Site)
